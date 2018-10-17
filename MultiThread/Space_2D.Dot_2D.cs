a1723e75d3773b12569d88e20113d3946ae2118a
Kirill <kir.mbr@gmail.com>
2018-10-17 18:49:21 +0300

    2D generating

---
+++ MultiThread/Space_2D.Dot_2D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MultiThread
+{
+    partial class Space_2D
+    {
+        public class Dot_2D
+        {
+            public float x { get; set; }
+            public float y { get; set; }
+
+            public Dot_2D(float x, float y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public Dot_2D()
+            {
+                x = 0;
+                y = 0;
+            }
+
+            public Dot_2D(Random random)
+            {
+                x = (float)random.NextDouble() * 100;
+                y = (float)random.NextDouble() * 100;
+            }
+
+            public Dot_2D(int _size, Random random)
+            {
+                x = (float)random.NextDouble() * _size;
+                y = (float)random.NextDouble() * _size;
+            }
+
+            public void print() => Console.WriteLine($"{x} | {y}");
+
+            public float Range(Dot_2D other) => (float)Math.Sqrt(Math.Pow(x - other.x, 2) + Math.Pow(y - other.y, 2));
+        }
+    }
+}
--- MultiThread/Space_3D.cs
+++ MultiThread/Space_3D.cs
@@ -8,61 +8,13 @@ using System.Threading;
 namespace MultiThread
 {
     //Трехмерная реализация задачи
-    public class Space_3D : Space
+    public partial class Space_3D : Space
     {
         public int Size { get; set; }
         public float Density { get; set; }
         const int THREAD_NUM = 4;
         Random rand = new Random(DateTime.Now.Millisecond);
         private List<Dot_3D> space = new List<Dot_3D>();
-
-
-        public class Dot_3D
-        {
-            public float x { get; set; }
-            public float y { get; set; }
-            public float z { get; set; }
-
-            
-
-            Dot_3D(float x, float y, float z)
-            {
-                this.x = x;
-                this.y = y;
-                this.z = z;
-            }
-
-            public Dot_3D(Random rand)
-            {
-                x = (float)rand.NextDouble() * 100;
-                y = (float)rand.NextDouble() * 100;
-                z = (float)rand.NextDouble() * 100;
-            }
-
-            public Dot_3D(int _size, Random rand)
-            {
-                x = (float)rand.NextDouble() * _size;
-                y = (float)rand.NextDouble() * _size;
-                z = (float)rand.NextDouble() * _size;
-            }
-
-            public Dot_3D()
-            {
-                x = 0;
-                y = 0;
-                z = 0;
-            }
-
-            public void print()
-            {
-                Console.WriteLine($"{x} | {y} | {z}");
-            }
-
-            public float Range(Dot_3D other)
-            {
-                return (float)Math.Sqrt(Math.Pow(x - other.x, 2) + Math.Pow(y - other.y, 2) + Math.Pow(z - other.z, 2));
-            }
-        }
 
         public Space_3D()
         {
